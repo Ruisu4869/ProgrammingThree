29a8453a23714068d077197925eac4f72c3b8684
Luis German Henao <lghenao85654@umanizales.edu.co>
2021-02-19 11:06:24 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sales-System
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
